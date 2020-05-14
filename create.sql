c3797ad780f7287ad68466c91b0108d6e67c5b5c
xiewenhua <vievenvua@gmail.com>
2020-05-15 01:22:42 +0800

    创建数据库表、视图的文件

---
+++ create.sql
@@ -0,0 +1,37 @@
+-- create database houses;
+-- use houses;
+CREATE TABLE sz_house(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  create_time datetime DEFAULT now(),
+  houselink varchar(255),
+  title varchar(255),
+  price int(11),
+  price_per_square int(11),
+  community_name varchar(255),
+  area varchar(255),
+  linknumber bigint(11),
+  housing_type varchar(255),
+  construction_area int(11),
+  inside_area int(11),
+  house_orientation varchar(255),
+  renovation_condition varchar(255),
+  equipped_with_elevator bool,
+  floor_area varchar(255),
+  total_floor int(11),
+  unit_structure varchar(255),
+  building_structure varchar(255),
+  staircase_ratio varchar(255),
+  room_spare_parts varchar(255),
+  owner_ship varchar(255),
+  usage_of_houses varchar(255),
+  transaction_ownership varchar(255),
+  housing_society_code varchar(255),
+  mortgage_information varchar(255),
+  housing_years varchar(255),
+  last_transaction date,
+  listing_time date,
+  PRIMARY KEY (id)
+);
+-- 创建显示主要数据视图
+DROP VIEW debug;
+CREATE VIEW debug_view  AS SELECT area AS `行政区`, price AS `总价`,price_per_square AS `每平价格`,construction_area AS `建筑面积`,inside_area AS `套内面积`,total_floor AS `总楼层`,equipped_with_elevator AS `电梯`,floor_area AS `楼层区域`,create_time AS `创建时间`,houselink AS `链接` FROM sz_house;
